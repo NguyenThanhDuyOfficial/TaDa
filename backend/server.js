012ab67b1d4ae97dc227b535640fa189be0520f4
NguyenThanhDuyOfficial <nguyenthanhduyofficial@gmail.com>
2026-04-20 15:56:54 +0700

    feat(back): init Node + Express

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('ERP API Ready'));
+app.listen(3000, () => console.log('Server on 3000'));
